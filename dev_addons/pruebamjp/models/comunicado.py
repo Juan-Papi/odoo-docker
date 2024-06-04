1b7f740f9a7150ba62f3ada9109fb1ce82a3b15f
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2024-06-04 15:38:17 -0400

    bd relacionada con usuario , no hay data en las vistas

--- dev_addons/pruebamjp/models/comunicado.py
+++ dev_addons/pruebamjp/models/comunicado.py
@@ -1,12 +1,13 @@
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class comunicado(models.Model):
-#     _name = 'pruebamjp.comunicado'
-#     _description = 'Modelo o tabla comunicado'
+class comunicado(models.Model):
+    _name = 'pruebamjp.comunicado'
+    _description = 'Modelo o tabla comunicado'
 
-#     nombre = fields.Char(required=True)
-#     description = fields.Text(required=True)
-#     fecha = fields.Datetime(required=True)
+    nombre = fields.Char(required=True)
+    description = fields.Text(required=True)
+    fecha = fields.Datetime(required=True)
+    comunicado_usuario_ids = fields.One2many('pruebamjp.comunicado_usuario', 'comunicado_id', string="Comunicados")
    
--- dev_addons/pruebamjp/models/comunicado_usuario.py
+++ dev_addons/pruebamjp/models/comunicado_usuario.py
@@ -1,10 +1,20 @@
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class comunicado_usuario(models.Model):
-#     _name = 'pruebamjp.comunicado_usuario'
-#     _description = 'Modelo o tabla comunicado_usuario'
+class comunicado_usuario(models.Model):
+    _name = 'pruebamjp.comunicado_usuario'
+    _description = 'Modelo o tabla comunicado_usuario'
 
-#     visto = fields.Char(required=True)
+    visto = fields.Char(required=True)
+    comunicado_id = fields.Many2one('pruebamjp.comunicado', string="Comunicado", ondelete='cascade', required=True)
+    usuario_recibe_id = fields.Many2one('res.users', string='UsuarioRecibe', default=lambda self: self.env.user, required=True)
+    usuario_envia_id = fields.Many2one('res.users', string='UsuarioEnvia', default=lambda self: self.env.user)
 
+    @api.model
+    def default_get(self, fields_list):
+        res = super(comunicado_usuario, self).default_get(fields_list)
+        user = self.env.user
+        if 'usuario_envia_id' in fields_list:
+            res['usuario_envia_id'] = user.id
+        return res
