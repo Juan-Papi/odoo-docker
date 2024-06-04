ab8f02da177ca39937a354e450c1fdc2256fdb91
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2024-06-04 14:55:35 -0400

    avance gral

---
+++ dev_addons/pruebamjp/models/comunicado.py
@@ -0,0 +1,12 @@
+
+# from odoo import models, fields, api
+
+
+# class comunicado(models.Model):
+#     _name = 'pruebamjp.comunicado'
+#     _description = 'Modelo o tabla comunicado'
+
+#     nombre = fields.Char(required=True)
+#     description = fields.Text(required=True)
+#     fecha = fields.Datetime(required=True)
+   
---
+++ dev_addons/pruebamjp/models/comunicado_usuario.py
@@ -0,0 +1,10 @@
+
+# from odoo import models, fields, api
+
+
+# class comunicado_usuario(models.Model):
+#     _name = 'pruebamjp.comunicado_usuario'
+#     _description = 'Modelo o tabla comunicado_usuario'
+
+#     visto = fields.Char(required=True)
+
